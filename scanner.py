773462c3b38c432f155e955d2a3c74e971340671
U.H. Chathurya Hewage <111672583+hewageuhcu@users.noreply.github.com>
2024-09-03 19:45:56 +0900

    added main identifiers

--- scanner.py
+++ scanner.py
@@ -1,3 +1,12 @@
 import cv2
 import numpy as np
 import utils
+
+##################################################
+webCamFeed=True
+pathImage=
+cap=cv2.VideoCapture(1)
+cap.set(10,160)
+heightImg=640
+widthImg=480
+##################################################
